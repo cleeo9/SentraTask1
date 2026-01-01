ddc97250cdb2415c9568d94818be5e3b2331a076
Leonel Cruz <leonelcruz20010@gmail.com>
2026-01-01 15:49:32 -0300

    modificacion login step

--- src/test/java/steps/LogInSteps.java
+++ src/test/java/steps/LogInSteps.java
@@ -1,21 +1,26 @@
 package steps;
 
 import org.testng.Assert;
-
 import io.cucumber.java.en.Then;
-
 import pages.MainPage;
 
 public class LogInSteps {
+
     MainPage landingPage = new MainPage();
 
-    @Then("Ingresa logueado a la plataforma  exitosamente")
-    public void se_muestra_mensaje() {
-        Assert.assertTrue(landingPage.isLoginSuccessful(), "Usuario no logueado correctamente");
+    @Then("Ingresa logueado a la plataforma exitosamente")
+    public void loginSuccess() {
+        Assert.assertTrue(
+                landingPage.isLoginSuccessful(),
+                "El usuario debería ingresar correctamente a la plataforma"
+        );
     }
 
     @Then("No logro ingresar a la plataforma")
-    public void se_muestra_mensaje_error() {
-        Assert.assertTrue(landingPage.isLoginFailed(), "Usuario logueado correctamente");
+    public void loginFail() {
+        Assert.assertFalse(
+                landingPage.isLoginSuccessful(),
+                "El usuario NO debería ingresar correctamente a la plataforma"
+        );
     }
 }
